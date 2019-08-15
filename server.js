86559461ff2088148d8c091bb34b87c650ea6262
Brian Zelip <bzelip@gmail.com>
2019-08-15 06:46:56 -0400

    server.js: Create file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
