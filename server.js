23400ee72205abdeaf78fb64c1b01571497b3e47
Brian Zelip <bzelip@gmail.com>
2019-08-15 06:57:36 -0400

    server.js: Create

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -8,8 +9,13 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.render('index');
+  response.sendFile(__dirname + '/index.html');
 });
+
+// // http://expressjs.com/en/starter/basic-routing.html
+// app.get('/', function(request, response) {
+//   response.render('index');
+// });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
