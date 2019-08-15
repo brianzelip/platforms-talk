a5a009b2c9fca226adc365e112ae2117441b8104
Brian Zelip <bzelip@gmail.com>
2019-08-15 08:27:56 -0400

    ./: Vueify repo

---
+++ components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <section>
+      <h1 class="title">
+        <a
+          href="https://github.com/brianzelip/platforms-talk"
+        >Platforms for Using Modern Web Dev Tools</a>
+      </h1>
+      <h2 class="event">
+        <a href="https://wiki.code4lib.org/MDC/2019">Code{4}Lib MDC 2019</a>
+        &middot; University of Baltimore
+      </h2>
+      <h2 class="author">
+        Brian Zelip &middot;
+        <a href="https://zelip.me">zelip.me</a>
+      </h2>
+    </section>
+  </main>
+</template>
